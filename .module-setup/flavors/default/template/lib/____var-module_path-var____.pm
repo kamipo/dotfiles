f3b71b408e6a62e1733e774cf99ce7ce96bb4c00
kamipo <kamipo@gmail.com>
2009-10-02 23:57:12 +0900

    added .module-setup

---
+++ .module-setup/flavors/default/template/lib/____var-module_path-var____.pm
@@ -0,0 +1,33 @@
+package [% module %];
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+[% module %] -
+
+=head1 SYNOPSIS
+
+  use [% module %];
+
+=head1 DESCRIPTION
+
+[% module %] is
+
+=head1 AUTHOR
+
+[% config.author %] E<lt>[% config.email %]E<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
