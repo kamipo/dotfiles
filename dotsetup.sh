d65d91c315ff88f2bddca1fe0183dbb29b875283
kamipo <kamipo@gmail.com>
2010-07-07 21:43:45 +0900

    added .ackrc

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-for dotfile in .gemrc .gdbinit .gitignore .dh-make-perl .module-setup .zshrc .zprofile .screenrc .vimrc .vim .toprc .inputrc
+for dotfile in .ackrc .gemrc .gdbinit .gitignore .dh-make-perl .module-setup .zshrc .zprofile .screenrc .vimrc .vim .toprc .inputrc
 do
   rm -fr ~/$dotfile
   ln -s $PWD/$dotfile ~/$dotfile
