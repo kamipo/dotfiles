5089fe77a0dfc486de06b1b7715b334364ade181
kamipo <kamipo@gmail.com>
2010-08-12 18:32:08 +0900

    binをシンボリックリンクにした

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,10 +1,7 @@
 #!/bin/sh -x
 
-for dotfile in .ackrc .gemrc .gdbinit .gitignore .dh-make-perl .module-setup .zshrc .zprofile .screenrc .vimrc .vim .toprc .inputrc
+for dotfile in .ackrc .gemrc .gdbinit .gitignore .dh-make-perl .module-setup .zshrc .zprofile .screenrc .vimrc .vim .toprc .inputrc bin
 do
   rm -fr ~/$dotfile
   ln -s $PWD/$dotfile ~/$dotfile
 done
-
-mkdir -p ~/bin
-cp -a $PWD/bin/* ~/bin
