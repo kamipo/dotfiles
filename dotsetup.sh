5a84d279b865cf48ed93e8d71f12d0a34db78697
Ryuta Kamizono <kamipo@gmail.com>
2012-04-17 17:22:58 +0900

    Update dotsetup.sh

--- dotsetup.sh
+++ dotsetup.sh
@@ -1,14 +1,14 @@
-#!/bin/sh -x
+#!/bin/bash
 
 git submodule init
 git submodule update
 
 cd .vim/autoload
-ln -sf ../bundle/vim-pathogen/autoload/pathogen.vim .
+ln -vnfs ../bundle/vim-pathogen/autoload/pathogen.vim .
 cd -
 
 for dotfile in .ackrc .caprc .gdbinit .gemrc .inputrc .pryrc .railsrc .screenrc .toprc .vim .vimrc .zshrc .zprofile
 do
   rm -fr ~/$dotfile
-  ln -nfs $PWD/$dotfile ~/$dotfile
+  ln -vnfs $PWD/$dotfile ~/$dotfile
 done
