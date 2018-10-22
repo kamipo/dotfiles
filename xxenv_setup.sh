a29965964330f912d7cebe624c0383a35a21641a
Ryuta Kamizono <kamipo@gmail.com>
2018-10-22 17:26:48 +0900

    Update xxenv_setup.sh
    * Update rbenv repo
    * Update Ruby version

--- xxenv_setup.sh
+++ xxenv_setup.sh
@@ -7,16 +7,16 @@ if [ ! -d ~/.rbenv ]; then
   ln -nfs $PWD/default-gems ~/.rbenv/default-gems
   mkdir -p ~/.rbenv/plugins
   cd ~/.rbenv/plugins
-  git clone https://github.com/sstephenson/ruby-build.git
-  git clone https://github.com/sstephenson/rbenv-default-gems.git
+  git clone https://github.com/rbenv/ruby-build.git
+  git clone https://github.com/rbenv/rbenv-default-gems.git
   git clone https://github.com/rkh/rbenv-update.git
 
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
 
   # ruby required libssl-dev libreadline6-dev libncurses5-dev libsqlite3-dev
-  rbenv install 2.4.2 --keep
-  rbenv global  2.4.2
+  rbenv install 2.5.3 --keep
+  rbenv global  2.5.3
   rbenv rehash
 fi
 
